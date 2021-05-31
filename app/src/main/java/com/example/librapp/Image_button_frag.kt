2ad58fccc8d7dfa29082052c179526f52346cfcc
asarzisartori <andrea.sarzisartori96@gmail.com>
2021-05-31 12:54:58 +0200

    Versione 1.0.2 (Aggiunta classe FreeAccess)

---
+++ app/src/main/java/com/example/librapp/Image_button_frag.kt
@@ -0,0 +1,19 @@
+package com.example.librapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_image_button_frag.*
+
+class Image_button_frag : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var v = inflater.inflate(R.layout.fragment_image_button_frag, container, false)
+        Button uno
+        return v
+        }
+    }
--- app/src/main/java/com/example/librapp/MainActivity.kt
+++ app/src/main/java/com/example/librapp/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.librapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun checkLogin (v: View?) {
+        val email = username_view.getText().toString()
+        if (!isNotValidEmail(email)) {
+            username_view.setError(getString(R.string.invalid_email))
+            Toast.makeText(applicationContext, "Pommerda", Toast.LENGTH_SHORT).show()
+        }
+
+        val pass = password_view.getText().toString()
+        if (!isNotValidPassword(pass)) {
+            password_view.setError(getString(R.string.invalid_password))
+        }
+    }
+
+    private fun isNotValidPassword(pass: String): Boolean {
+        return if (pass != null && pass.length >= 8) {
+            true
+        } else {
+            false
+        }
+    }
+
+    private fun isNotValidEmail(email: String): Boolean {
+        val EMAIL_PATTERN = ("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
+        val pattern = Pattern.compile(EMAIL_PATTERN)
+        val matcher = pattern.matcher(email)
+        return matcher.matches()
+    }
+
+    fun openFreeAccess(v: View){
+        val intent = Intent(this@MainActivity, FreeAccess::class.java)
+        startActivity(intent)
+    }
+
 }
