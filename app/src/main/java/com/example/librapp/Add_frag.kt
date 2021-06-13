d6d969afaceb4ba214862518808159b2d7d1cb38
scervini <samuele.cervini3@gmail.com>
2021-06-13 22:18:53 +0200

    Versione(1.0.9) Aggiunto inserimento dati nel database

--- app/src/main/java/com/example/librapp/Add_frag.kt
+++ app/src/main/java/com/example/librapp/Add_frag.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class Add_frag : Fragment() {
 
@@ -15,7 +18,37 @@ class Add_frag : Fragment() {
        val view = inflater.inflate(R.layout.fragment_add_frag, container, false)
 
         view.findViewById<Button>(R.id.button_AddProduct).setOnClickListener {
-            Toast.makeText(context, "dio bono", Toast.LENGTH_SHORT).show()
+            val titolo = view.findViewById<EditText>(R.id.titleA).text.toString()
+            val autore = view.findViewById<EditText>(R.id.authorA).text.toString()
+            val genere = view.findViewById<EditText>(R.id.genereA).text.toString()
+            val tipologia = view.findViewById<EditText>(R.id.typoA).text.toString()
+            val descrizione = view.findViewById<EditText>(R.id.descriptionA).text.toString()
+            if(titolo.isEmpty()){
+                view.findViewById<EditText>(R.id.titleA).error = getString(R.string.invalid_username)
+
+            }
+            if(autore.isEmpty()){
+                view.findViewById<EditText>(R.id.authorA).error = getString(R.string.invalid_username)
+            }
+            if(genere.isEmpty()){
+                view.findViewById<EditText>(R.id.genereA).error = getString(R.string.invalid_username)
+            }
+            if(tipologia.isEmpty()){
+                view.findViewById<EditText>(R.id.typoA).error = getString(R.string.invalid_username)
+            }
+            if(descrizione.isEmpty()){
+                view.findViewById<EditText>(R.id.descriptionA).error = getString(R.string.invalid_username)
+            }
+            val item = Item(titolo, autore, genere, tipologia, descrizione)
+            FirebaseDatabase.getInstance().getReference("Item").child(tipologia).child(titolo).setValue(item)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(context, "Item Successfully uploaded", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
         }
 
         return view
--- app/src/main/java/com/example/librapp/Item.kt
+++ app/src/main/java/com/example/librapp/Item.kt
@@ -1,3 +1,4 @@
 package com.example.librapp
 
 data class Item(val titolo: String, val autor: String, val genere: String, val tipologia: String, val descrizione: String)
+
